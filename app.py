9fa09e3bfb6e8966294504486ec28f051e260739
takehiro <takehiro-r133@hotmail.co.jp>
2024-02-24 11:01:57 +0900

    connect with ChatGPT

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import re
+from langchain_community.chat_models import ChatOpenAI
 
 load_dotenv()
 
@@ -10,9 +12,15 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 # メンションされたらsay
 @app.event("app_mention")
 def message_mention(event, say):
-  user = event["user"]
   thread_ts = event["ts"]
-  say(thread_ts=thread_ts, text=f"Hello <@{user}>!")
+  message = re.sub("<@.*>","",event["text"])
+
+  llm = ChatOpenAI(
+    model_name=os.environ["OPENAI_API_MODEL"],
+    temperature=os.environ["OPENAI_API_TEMPERATURE"]
+  )
+  response = llm.predict(message)
+  say(text=response, thread_ts=thread_ts)
 
 # アプリを起動します
 if __name__ == "__main__":
