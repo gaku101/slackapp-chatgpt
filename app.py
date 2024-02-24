704e2b37d9326cd150972a02318eb2cfb949c60a
takehiro <takehiro-r133@hotmail.co.jp>
2024-02-24 10:55:11 +0900

    connect with slack app

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ load_dotenv()
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-if __name__=="__main__":
+# メンションされたらsay
+@app.event("app_mention")
+def message_mention(event, say):
+  user = event["user"]
+  thread_ts = event["ts"]
+  say(thread_ts=thread_ts, text=f"Hello <@{user}>!")
+
+# アプリを起動します
+if __name__ == "__main__":
   SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
